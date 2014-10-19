89954d85228f85e3dfc6eca22048ca19e5efac75
Mustafa <mus@musalbas.com>
2014-10-19 19:53:34 +0100

    Force Flash mode on Firefox to prevent crashes.

--- listentotwitter/static/js/notes.js
+++ listentotwitter/static/js/notes.js
@@ -1,12 +1,19 @@
 var soundManagerReady = false;
+var soundManagerPreferFlash = false;
 var notes = {}
+
+if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+    soundManagerPreferFlash = true;
+}
 
 soundManager.setup({
     url: '/static/swf/',
-    preferFlash: false,
+    preferFlash: soundManagerPreferFlash,
+    flashVersion: 9,
+    useHighPerformance: true,
     onready: function() {
         soundManagerReady = true;
-    }
+    },
 });
 
 function playNote(note) {
